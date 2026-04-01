e0a3cbb97fe0704196a4126029d6a7412199ebab
Dagemkasahun <Dagemkasahun2@gmail.com>
2026-04-01 16:17:14 +0300

    Merged sub-repository into main project

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // You might need to install 'path' if not present
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
+    alias: {
+      // This tells Vite that '@' points to your 'src' folder
+      '@': path.resolve(__dirname, './src'),
+    },
     extensions: ['.js', '.ts', '.jsx', '.tsx']
   },
-  plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: false,
